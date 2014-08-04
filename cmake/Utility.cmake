1f9cd26ef51bf0a82b5b449ff37e6816257b2dbe
Tim Benedict Jagla <tim@nptricky.com>
2014-08-04 17:11:34 +0200

    integrated eigen thirdparty lib into project

--- cmake/Utility.cmake
+++ cmake/Utility.cmake
@@ -41,7 +41,7 @@ endfunction()
 # -----------------------------------------------------------------------------
 #
 # OUTPUT
-# - FATAL_ERROR on unsupported compiler
+# - FATAL_ERROR on unsupported c++11
 #
 # -----------------------------------------------------------------------------
 function(check_compiler)
@@ -73,17 +73,17 @@ endfunction()
 # INPUT
 # - FOLDER
 #     the folder to glob recursively
-# - (name) OUTPUT_FILE_LIST
-# - (name) OUTPUT_INCLUDE_DIR_LIST
+# - (name) OUTPUT_INCLUDE_FILES
+# - (name) OUTPUT_INCLUDE_DIR
 #
 # OUTPUT
-# - OUTPUT_FILE_LIST
+# - OUTPUT_INCLUDE_FILES
 #     the list of files extracted recursively from the folder
-# - OUTPUT_INCLUDE_DIR_LIST
+# - OUTPUT_INCLUDE_DIR
 #     the list of directories containing *.h files
 #
 # -----------------------------------------------------------------------------
-function(glob_source_group FOLDER OUTPUT_FILE_LIST OUTPUT_INCLUDE_DIR_LIST)
+function(glob_source_group FOLDER OUTPUT_INCLUDE_FILES OUTPUT_INCLUDE_DIR)
   # base path wont be evaluated by source_group()
   set(PATH_BASE "${FOLDER}")
   file(GLOB_RECURSE ALL_FILES ${FOLDER}/*)
@@ -123,8 +123,8 @@ function(glob_source_group FOLDER OUTPUT_FILE_LIST OUTPUT_INCLUDE_DIR_LIST)
   # remove unneeded duplicates from the include directory list
   # list(REMOVE_DUPLICATES INCLUDE_DIR_LIST)
   
-  set(${OUTPUT_FILE_LIST} "${FILE_LIST}" PARENT_SCOPE)
-  set(${OUTPUT_INCLUDE_DIR_LIST} "${INCLUDE_DIR_LIST}" PARENT_SCOPE)
+  set(${OUTPUT_INCLUDE_FILES} "${FILE_LIST}" PARENT_SCOPE)
+  set(${OUTPUT_INCLUDE_DIR} "${INCLUDE_DIR_LIST}" PARENT_SCOPE)
 endfunction()
 
 # -----------------------------------------------------------------------------
@@ -138,17 +138,16 @@ function(print_cmake_variables)
 endfunction()
 
 # -----------------------------------------------------------------------------
-# print_cmake_variables
+# print_cxx_known_features
 # -----------------------------------------------------------------------------
 function(print_cxx_known_features)
-  message("Out of all possible C++ compiler features CMake knows about:")
+  message("CMAKE_CXX_KNOWN_FEATURES:")
   foreach(FEATURE ${CMAKE_CXX_KNOWN_FEATURES})
-    message("  ${FEATURE}")
+    message(" - ${FEATURE}")
   endforeach()
-  message("")
-  message("your C++ compiler supports these features")
+  message("CMAKE_CXX_COMPILE_FEATURES:")
   foreach(FEATURE ${CMAKE_CXX_COMPILE_FEATURES})
-    message("  ${FEATURE}")
+    message(" - ${FEATURE}")
   endforeach()
 endfunction()
 
@@ -168,4 +167,84 @@ function(check_configuration_generator)
   else()
     message(STATUS "Multi-Configuration Generator > CMAKE_CONFIGURATION_TYPES: ${CMAKE_CONFIGURATION_TYPES}")
   endif()
+endfunction()
+
+# -----------------------------------------------------------------------------
+# check_source_build
+# -----------------------------------------------------------------------------
+#
+# OUTPUT
+# - FATAL_ERROR on in-source build
+#
+# -----------------------------------------------------------------------------
+function(check_source_build)
+  if(${CMAKE_SOURCE_DIR} STREQUAL ${CMAKE_BINARY_DIR})
+    message(FATAL_ERROR "In-source builds not allowed. Please make a new directory (called a build directory) and run CMake from there. You may need to remove CMakeCache.txt.")
+  endif()
+endfunction()
+
+# -----------------------------------------------------------------------------
+# string_first_char_to_upper_case
+# -----------------------------------------------------------------------------
+#
+# INPUT
+# - INPUT_STRING
+#     string to make first character upper case
+# - (name) OUTPUT_STRING
+#
+# OUTPUT
+# - OUTPUT_STRING
+#     string with first character in upper case
+#
+# -----------------------------------------------------------------------------
+function(string_first_char_to_upper_case INPUT_STRING OUTPUT_STRING)
+  string(SUBSTRING "${INPUT_STRING}" 0  1 FIRST)
+  string(SUBSTRING "${INPUT_STRING}" 1 -1 TAIL )
+  string(TOUPPER "${FIRST}" FIRST)
+  set(${OUTPUT_STRING} "${FIRST}${TAIL}" PARENT_SCOPE)
+endfunction()
+
+# -----------------------------------------------------------------------------
+# string_underscore_to_camel_case
+# -----------------------------------------------------------------------------
+#
+# INPUT
+# - INPUT_STRING
+#     underscore separated string about to be in camel case
+# - (name) OUTPUT_STRING
+#
+# OUTPUT
+# - OUTPUT_STRING
+#     camel case string
+#
+# -----------------------------------------------------------------------------
+function(string_underscore_to_camel_case INPUT_STRING OUTPUT_STRING)
+  string(REPLACE "_" ";" INPUT_STRING_LIST ${INPUT_STRING})
+  foreach(SUBSTRING ${INPUT_STRING_LIST})
+    string_first_char_to_upper_case(SUBSTRING SUBSTRING_UPPER)
+    set(CAMEL_CASE ${CAMEL_CASE}${SUBSTRING_UPPER})
+  endforeach()
+  set(${OUTPUT_STRING} ${CAMEL_CASE} PARENT_SCOPE)
+endfunction()
+
+# -----------------------------------------------------------------------------
+# get_library_dir_from_current_source_dir
+# -----------------------------------------------------------------------------
+#
+# INPUT
+# - (name) OUTPUT_PATH
+#
+# OUTPUT
+# - OUTPUT_PATH
+#     path to the respective library
+#
+# -----------------------------------------------------------------------------
+function(get_library_dir_from_current_source_dir OUTPUT_PATH)
+  # retrieve library name from directory name
+  get_filename_component(CURRENT_DIR "${CMAKE_CURRENT_SOURCE_DIR}" NAME)
+  string(REGEX REPLACE "[.].*$" "" CURRENT_LIBRARY_NAME "${CURRENT_DIR}")
+  # retrieve library directory from library name
+  get_filename_component(PARENT_DIR "${CMAKE_CURRENT_SOURCE_DIR}" DIRECTORY)
+  set(CURRENT_LIBRARY_DIR "${PARENT_DIR}/${CURRENT_LIBRARY_NAME}")
+  set(${OUTPUT_PATH} ${CURRENT_LIBRARY_DIR} PARENT_SCOPE)
 endfunction()
